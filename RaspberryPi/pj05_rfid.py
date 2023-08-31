830c1b66e25c8bb63f1c12b762ed481fcef9f2c9
yangke0227@gmail.com <yangke0227@gmail.com>
2023-08-31 17:10:36 +0800

    UPDATE@OFFICE

---
+++ RaspberryPi/pj05_rfid.py
@@ -0,0 +1,16 @@
+# (For Pi)https://pypi.org/project/mfrc522/
+# (For Pico)https://github.com/kevinmcaleer/pico-rfid
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
