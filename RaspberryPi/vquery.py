45f3a3a186b3a8bc8279f03023b3e9c65862abaa
yangke0227@gmail.com <yangke0227@gmail.com>
2023-09-07 11:46:57 +0800

    UPDATE@HOME

---
+++ RaspberryPi/vquery.py
@@ -0,0 +1,23 @@
+﻿import sys
+import pymysql
+
+def connDb():
+  # 打开数据库连接
+  db = pymysql.connect(host='192.168.1.72', user='root', password='XXXXXXX', database='gravity', port=9211)
+  # 使用 cursor() 方法创建一个游标对象 cursor
+  # cursor = db.cursor()
+  return db
+
+def queryDb(db, sql):
+  cursor = db.cursor()
+  cursor.execute(sql)
+  data = cursor.fetchall()
+  return data
+  
+if len(sys.argv) > 1:
+  db = connDb()
+  print(str(sys.argv[1]))
+  sql = "SELECT * FROM tb_fit WHERE vcode LIKE '%%%s%%'" % str(sys.argv[1])
+  vinfo = queryDb(db, sql)
+  print(vinfo)
+  db.close()
