b140151d4977a635d4bd5c0c26a530c248aaa58c
yangke0227@gmail.com <yangke0227@gmail.com>
2023-09-27 08:46:57 +0800

    UPDATE@OFFICE

--- RaspberryPico/MicroPython/pj07_ds1302_clock.py
+++ RaspberryPico/MicroPython/pj07_ds1302_clock.py
@@ -1,3 +1,8 @@
 # TODO ds1302 时钟模块
 # https://blog.csdn.net/weixin_52168861/article/details/125212859
 # https://github.com/LNfromNorth/DesktopClock/tree/main/OtherPy/DS1302
+from DS1302 import DS1302
+
+ds1302 = DS1302(0,1,2)
+ds1302.SetTime(22,6,8,22,15,00,3)
+date = ds1302.Now()
