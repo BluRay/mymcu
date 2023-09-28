8afd9f588a1ec854b1d2a97a217ebe228cac63cc
yangke0227@gmail.com <yangke0227@gmail.com>
2023-09-28 10:34:16 +0800

    UPDATE@OFFICE

--- RaspberryPico/MicroPython/pj07_ds1302_clock.py
+++ RaspberryPico/MicroPython/pj07_ds1302_clock.py
@@ -1,4 +1,5 @@
 # TODO ds1302 时钟模块
+# 设置当前时间 读取时间显示到墨水屏【EPD_2in9_B】
 # https://blog.csdn.net/weixin_52168861/article/details/125212859
 # https://github.com/LNfromNorth/DesktopClock/tree/main/OtherPy/DS1302
 from DS1302 import DS1302
