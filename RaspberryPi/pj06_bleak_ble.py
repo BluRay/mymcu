4725881e0f21fcfec6d5d1ab9701dd6a4f7b4785
yangke0227@gmail.com <yangke0227@gmail.com>
2023-10-20 13:31:50 +0800

    UPDATE@OFFICE

--- RaspberryPi/pj06_bleak_ble.py
+++ RaspberryPi/pj06_bleak_ble.py
@@ -21,6 +21,19 @@ async def getBleModelNumber(address):
   async with BleakClient(address) as client:
     model_number = await client.read_gatt_char(MODEL_NBR_UUID)
     print("Model Number: {0}".format("".join(map(chr, model_number))))
+
+# 获取设备服务
+async def getBleService(address):
+  async with BleakClient(address) as client:
+    print('-->Client Success')
+    services = client.services
+    for service in services:
+      print("Service UUID:", service.uuid)
+      for character in service.characteristices:
+        print("uuid:", character.uuid)
+        print("properties:", character.properties)
+  print("disConnected")
+  
     
 # 发送数据
 # 设备蓝牙地址
@@ -29,9 +42,9 @@ ADDRESS = "A0:9F:10:7E:1B:5D"
 UART_RX_CHAR_UUID = "49535343-8830-43F4-A8D4-ECBE34729B77"
 UART_TX_CHAR_UUID = "49535343-1E4D-4BD9-BA61-23C647249677"
 # 发送数据
-openDoor_send = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+data_send = [0x00, 0x11, 0x22]
 # 返回数据
-openDoor_recive = [0x00, 0x00, 0x00, 0x00, 0x00, 0x0]
+data_recive = [0x00, 0x00, 0x00, 0x00, 0x00, 0x0]
 
 async def sent(address, data):
   async with BleakClient(address) as client:
@@ -42,7 +55,7 @@ async def sent(address, data):
         while paired == True:
           print("send data")
           await client.write_gatt_char(UART_RX_CHAR_UUID,bytes(data))
-          await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
+          #await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
           await asyncio.sleep(1.0)
 
 ## START ##
